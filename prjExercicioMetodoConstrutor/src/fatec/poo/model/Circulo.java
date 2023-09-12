f329301f39cbbb386bea279883044911cdf49024
joao-ghizzi <111469423+joao-ghizzi@users.noreply.github.com>
2023-09-12 11:34:16 -0300

    Add files via upload

---
+++ prjExercicioMetodoConstrutor/src/fatec/poo/model/Circulo.java
@@ -0,0 +1,36 @@
+package fatec.poo.model;
+
+
+public class Circulo {
+    private double raio;
+    private String unidadeMedida;
+
+    //método do construtor    
+
+    public Circulo(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+    
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+    
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }   
+    
+    public double calcArea(){
+        return(Math.PI * Math.pow(raio,2));
+    }
+    public double calcPerimetro(){
+        return(2 * Math.PI * raio);
+    }
+    public double calcDiametro(){
+        return(2 * raio);
+    }    
+}
