0a61aef55e3a76aab6db60a7ef3edf3cdaa7a025
joao-ghizzi <111469423+joao-ghizzi@users.noreply.github.com>
2023-09-12 11:33:18 -0300

    Add files via upload

---
+++ prjCirculo/src/fatec/poo/model/Circulo.java
@@ -0,0 +1,25 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Dimas
+ */
+public class Circulo {
+    private double raio;
+    
+    public void setRaio(double r){
+        raio = r;
+    }      
+    public double getRaio(){
+        return(raio);
+    }    
+    public double calcArea(){
+        return (3.1416 * Math.pow(raio, 2));
+    }    
+    public double calcPerimetro(){
+        return (2 * 3.1416 * raio);
+    }
+    public double calcDiametro(){
+        return(2 * raio);
+    }
+}  
